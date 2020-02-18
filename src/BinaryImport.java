0ac6e2363985462b0fad4424041c25c2269c11e2
Viacheslav Kolpakov <viachakolpakov@yandex.ru>
2020-02-18 11:54:01 +0300

    изменены имена переменных и добавлены пробелы в операторах

--- src/BinaryImport.java
+++ src/BinaryImport.java
@@ -4,7 +4,7 @@ import static java.lang.Integer.parseInt;
 public class BinaryImport {
     public static void main (String[] args) {
         Scanner console = new Scanner(System.in);
-        String a = console.nextLine();
-        System.out.println(parseInt(a,2));
+        String binary = console.nextLine();
+        System.out.println(parseInt(binary,2));
     }
 }
--- src/multiplication.java
+++ src/multiplication.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 public class multiplication {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int a = console.nextInt();
-        System.out.println(Integer.toString(a) + " x 1 = " + Integer.toString(a * 1)+"\n" +
-                Integer.toString(a) + " x 2 = " + Integer.toString(a * 2)+"\n" +
-                Integer.toString(a) + " x 3 = " + Integer.toString(a * 3)+"\n" +
-                Integer.toString(a) + " x 4 = " + Integer.toString(a * 4)+"\n" +
-                Integer.toString(a) + " x 5 = " + Integer.toString(a * 5)+"\n" +
-                Integer.toString(a) + " x 6 = " + Integer.toString(a * 6)+"\n" +
-                Integer.toString(a) + " x 7 = " + Integer.toString(a * 7)+"\n" +
-                Integer.toString(a) + " x 8 = " +  Integer.toString(a * 8)+"\n" +
-                Integer.toString(a) + " x 9 = " +  Integer.toString(a * 9)+"\n" +
-                Integer.toString(a) + " x 10 = " +  Integer.toString(a * 10)+"\n");
+        int multiplier = console.nextInt();
+        System.out.println(Integer.toString(multiplier) + " x 1 = " + Integer.toString(multiplier * 1) + "\n" +
+                Integer.toString(multiplier) + " x 2 = " + Integer.toString(multiplier * 2) + "\n" +
+                Integer.toString(multiplier) + " x 3 = " + Integer.toString(multiplier * 3) + "\n" +
+                Integer.toString(multiplier) + " x 5 = " + Integer.toString(multiplier * 5) + "\n" +
+                Integer.toString(multiplier) + " x 6 = " + Integer.toString(multiplier * 6) + "\n" +
+                Integer.toString(multiplier) + " x 7 = " + Integer.toString(multiplier * 7) + "\n" +
+                Integer.toString(multiplier) + " x 8 = " +  Integer.toString(multiplier * 8) + "\n" +
+                Integer.toString(multiplier) + " x 9 = " +  Integer.toString(multiplier * 9) + "\n" +
+                Integer.toString(multiplier) + " x 10 = " +  Integer.toString(multiplier * 10) + "\n");
 
     }
 }
