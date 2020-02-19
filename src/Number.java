7143cec73678c8f3109cdbaacea2f5585535b26f
Viacheslav Kolpakov <viachakolpakov@yandex.ru>
2020-02-19 19:00:57 +0300

    задание номер 7

---
+++ src/Number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        System.out.println("Введите значение для переменной");
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+        int x = 2;
+        int y = 4;
+        int z = 6;
+
+        if(number == x || number == y || number == z ) {
+            System.out.println("Данное значение имеется в константах");
+        }
+        else {
+            System.out.println("Такой константы нет!");
+        }
+    }
+
+
+}
+            //else {
+        //System.out.println("Данное значение не найдено");
+
