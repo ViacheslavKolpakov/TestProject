f56e32dec3a3ecb5ae40b4ae5881a249d2d89ec0
Viacheslav Kolpakov <viachakolpakov@yandex.ru>
2020-02-20 18:17:10 +0300

    задание номер 8

---
+++ src/Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+    public static void main (String[] args){
+        int digit = 1;
+        while ( digit <= 50) {
+            System.out.println(digit);
+            digit++;
+        }
+    }
+}
